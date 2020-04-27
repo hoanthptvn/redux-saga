e6967d628704285174847d51db6656949e1d434a
Kien <trungkien2895@gmail.com>
2020-04-27 10:00:03 +0700

    create circle button icon, props icon in the props Fab

---
+++ src/components/TaskList/index.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+
+import TaskItem from './../TaskItem';
+
+import { withStyles } from '@material-ui/core';
+import Box from '@material-ui/core/Box';
+import Grid from '@material-ui/core/Grid';
+import styles from './styles';
+
+class TaskList extends Component {
+  render() {
+    const { classes, status, tasks, index } = this.props;
+    return (
+      <Grid item md={4} xs={12} key={index}>
+        <Box mt={2} mb={2}>
+          <div className={classes.status}>{status.label}</div>
+        </Box>
+        <div className={classes.wrapperListTask}>
+          {
+            tasks.map(task => {
+              return <TaskItem key={task.id} status={status} task={task} />
+            })
+          }
+        </div>
+      </Grid>
+    )
+  }
+}
+
+export default withStyles(styles)(TaskList);
--- src/containers/Taskboard/index.js
+++ src/containers/Taskboard/index.js
@@ -1,17 +1,15 @@
 import React, { Component } from 'react';
 import { withStyles } from '@material-ui/core';
 import styles from './styles';
+import TaskList from './../../components/TaskList'
 
 import { STATUSES } from './../../constants'
 
 import Button from "@material-ui/core/Button";
-import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import Typography from '@material-ui/core/Typography';
+
 import AddIcon from "@material-ui/icons/Add";
 import Grid from '@material-ui/core/Grid';
-import Box from '@material-ui/core/Box';
+
 
 const ListTask = [
   {
@@ -35,7 +33,6 @@ const ListTask = [
 class TaskBoard extends Component {
 
   renderBoard() {
-    const { classes } = this.props;
     let xhml = null;
     xhml = (
       <Grid container spacing={2}>
@@ -43,37 +40,7 @@ class TaskBoard extends Component {
         {
           STATUSES.map((status, index) => {
             const taskFiltered = ListTask.filter(task => task.status === status.value);
-            return (
-              <Grid item md={4} xs={12} key={index}>
-                <Box mt={2} mb={2}>
-                  <div className={classes.status}>{status.label}</div>
-                </Box>
-                <div className={classes.wrapperListTask}>
-                  {
-                    taskFiltered.map(task => {
-                      const { title, description } = task;
-                      return (
-                        <Card key={task.id} className={classes.card}>
-                          <CardContent>
-                            <Grid container justify="space-between">
-                              <Grid item md={8}>
-                                <Typography component="h2">{title}</Typography>
-                              </Grid>
-                              <Grid item md={4}>
-                                {status.label}
-                              </Grid>
-                            </Grid>
-                          </CardContent>
-                          <CardActions>
-                            <Button size="small"></Button>
-                          </CardActions>
-                        </Card>
-                      )
-                    })
-                  }
-                </div>
-              </Grid>
-            )
+            return <TaskList key={index} status={status} index={index} tasks={taskFiltered} />
           })
         }
       </Grid >
