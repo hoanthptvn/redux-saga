3a59e672b1fe067e32ad999ede960ee2eafccaf7
Kien <trungkien2895@gmail.com>
2020-04-29 11:06:45 +0700

    configuration, install eslint-plugin-react and eslint-config-airbnb

--- src/containers/Taskboard/index.js
+++ src/containers/Taskboard/index.js
@@ -1,67 +1,74 @@
-import React, { Component } from 'react';
 import { withStyles } from '@material-ui/core';
-import styles from './styles';
-import TaskList from './../../components/TaskList';
-import { STATUSES } from './../../constants';
-import TaskForm from './../../components/TaskForm';
-
 import Button from "@material-ui/core/Button";
-import AddIcon from "@material-ui/icons/Add";
 import Grid from '@material-ui/core/Grid';
-
-
+import AddIcon from "@material-ui/icons/Add";
+import PropTypes from 'prop-types';
+import React, { Component } from 'react';
+import TaskForm from '../../components/TaskForm';
+import TaskList from '../../components/TaskList';
+import { STATUSES } from '../../constants';
+import styles from './styles';
 
 const ListTask = [
   {
     id: 1,
     title: "Read book",
     description: "Ready material book",
-    status: 0
+    status: 0,
   }, {
     id: 2,
     title: "Play football",
     description: "With my friend",
-    status: 2
+    status: 2,
   }, {
     id: 3,
     title: "Play game",
     description: "Play game fifa",
-    status: 1
-  }
+    status: 1,
+  },
 ];
 
 class TaskBoard extends Component {
-
-  state = {
-    open: false
-  };
-
-  renderBoard() {
-    let xhtml = null;
-    xhtml = (
-      <Grid container spacing={2}>
-
-        {
-          STATUSES.map((status, index) => {
-            const taskFiltered = ListTask.filter(task => task.status === status.value);
-            return <TaskList key={index} status={status} index={index} tasks={taskFiltered} />;
-          })
-        }
-      </Grid >
-    );
-    return xhtml;
+  constructor(props) {
+    super(props);
+    this.state = {
+      open: false,
+    };
   }
 
   handleClose = () => {
     this.setState({
-      open: false
+      open: false,
     });
   }
 
   openForm = () => {
     this.setState({
-      open: true
+      open: true,
     });
+  }
+
+  renderBoard() {
+    let xhtml = null;
+    xhtml = (
+      <Grid
+        container
+        spacing={2}>
+        {
+          STATUSES.map((status, index) => {
+            const taskFiltered = ListTask.filter((task) => task.status === status.value);
+            return (
+              <TaskList
+                key={index}
+                status={status}
+                index={index}
+                tasks={taskFiltered} />
+            );
+          })
+        }
+      </Grid>
+    );
+    return xhtml;
   }
 
   renderForm() {
@@ -74,7 +81,9 @@ class TaskBoard extends Component {
   render() {
     const { classes } = this.props;
     return (
-      <div className={classes.taskBoard} >
+      <div
+        className={classes.taskBoard}
+      >
         <Button variant="contained" color="primary" className={classes.button} onClick={this.openForm}>
           <AddIcon /> Thêm mới công việc
         </Button>
@@ -85,5 +94,9 @@ class TaskBoard extends Component {
     );
   }
 }
+
+TaskBoard.propTypes = {
+  classes: PropTypes.object,
+};
 
 export default withStyles(styles)(TaskBoard);
